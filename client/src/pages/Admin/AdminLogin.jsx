fdf9a93c19a167be29aad454441509e6c4452dcc
ramananubhaw <anubhawraman@gmail.com>
2025-09-06 14:01:42 +0530

    Login page updated

--- client/src/pages/Admin/AdminLogin.jsx
+++ client/src/pages/Admin/AdminLogin.jsx
@@ -9,12 +9,12 @@ const AdminLogin = () => {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Admin Login
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
+          {/* <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
             <Link to="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
               login as student
             </Link>
-          </p>
+          </p> */}
         </div>
         <LoginForm isAdmin={true} />
       </div>
